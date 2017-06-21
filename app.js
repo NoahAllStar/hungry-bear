dc965ce9c3df7d64078e46c7c9c97533909d58a5
NoahAllStar <noahone808@gmail.com>
2017-06-20 14:58:18 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var arr = [false, undefined, "hello", 5, null];
+// Create animals array containing 5 animals
+console.log(arr);
+
+var arr = ["Meerkat", "Fox", "Lion", "Tiger", "Puppy"];
+console.log(arr);
+console.log(animals);
+// var animal = "Kae-Sn's Mom";
+// function runningawayfromKaesnsMom(Mom){
+//   if (Mom === "KaesnsMom"){
+//     console.log("It's ok, Kaesns Mom is lit");
+//   } else {
+//   console.log("running away from " + Mom);
+//   }
+// }
+// runningawayfromKaesnsMom(animal);
